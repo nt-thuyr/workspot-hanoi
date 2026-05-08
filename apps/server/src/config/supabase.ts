7ac29ec0c56dc574dacf256ab18d54120efc81bb
Liem Thanh <nk.tl.bn.2005@gmail.com>
2026-05-08 20:32:11 +0700

    Update supabase.ts

--- apps/server/src/config/supabase.ts
+++ apps/server/src/config/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-
+import * as dotenv from 'dotenv';
 // Supabase local development
+dotenv.config();
+
 const supabaseUrl = 'http://127.0.0.1:54321';
 const supabaseKey = 'sb_secret_N7UND0UgjKTVK-Uodkm0Hg_xSvEMPvz'; // service_role key
 
